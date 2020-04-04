4e6a6ebcc67ebf341541413d147ba8f36180dc35
kuchi <burner.0107@gmail.com>
2020-04-03 21:55:09 -0400

    mongoose

--- app.js
+++ app.js
@@ -1,39 +1,79 @@
 var express = require('express');
 var app = express()
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
+mongoose.set('debug', true);
+mongoose.connect('mongodb://localhost:27017/yelp_camp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Schema Setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+var Campground = mongoose.model('Campground', campgroundSchema);
 
 
-var campgrounds = [
-    {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8605/16573646931_22fc928bf9_o.jpg"},
-    {name: "Granite Hill", image: "https://q9m3bv0lkc15twiiq99aa1cy-wpengine.netdna-ssl.com/wp-content/uploads/2019/07/TENT.jpeg"},
-    {name: "Mountain Goat", image: "https://www.planetware.com/wpimages/2019/10/montana-glacier-national-park-best-campgrounds-fish-creek-campground.jpg"},
-    {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8605/16573646931_22fc928bf9_o.jpg"},
-    {name: "Granite Hill", image: "https://q9m3bv0lkc15twiiq99aa1cy-wpengine.netdna-ssl.com/wp-content/uploads/2019/07/TENT.jpeg"},
-    {name: "Mountain Goat", image: "https://www.planetware.com/wpimages/2019/10/montana-glacier-national-park-best-campgrounds-fish-creek-campground.jpg"},
-]
-
-
-app.get('/', function(req, res) {
+// GET
+app.get('/', function (req, res) {
     res.render('landing');
 });
 
-app.get('/campgrounds', function(req, res) {
-    res.render('campgrounds', {campgrounds: campgrounds});
+// GET
+app.get('/campgrounds', function (req, res) {
+    Campground.find({}, function (err, allCampgrounds) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('campgrounds', { campgrounds: allCampgrounds });
+        }
+    });
 });
 
-app.post('/campgrounds', function(req, res) {
+// POST
+app.post('/campgrounds', function (req, res) {
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image}
-    campgrounds.push(newCampground);
-    res.redirect('/campgrounds');
+
+    Campground.create(
+        { name: name, image: image },
+        function (err, newCampground) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.redirect('/campgrounds');
+            }
+        }
+    );
 });
 
-app.get('/campgrounds/new', function(req, res) {
+// NEW -form to create new campground
+app.get('/campgrounds/new', function (req, res) {
     res.render("new.ejs");
+});
+
+// GET
+app.get('/campgrounds/:id', function (req, res) {
+    var id = req.params.id;
+    Campground.findById(id)
+        .exec()
+        .then(result => {
+            res.status(200).json(
+                result
+            );
+        })
+        .catch(err => {
+            res.status(500).json({
+                err: err
+            });
+        })
 });
 
 app.listen(3000, () => {
